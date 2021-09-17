eb88c3a73c25e99bcb95d2fe4764078544d632fd
Danny Yang <dannyyaaj@gmail.com>
2021-09-16 22:57:21 -0500

    add more notes for callback function section

--- async-foundations/callback-functions.js
+++ async-foundations/callback-functions.js
@@ -1,5 +1,7 @@
 //callback function is a function that is passed into another function as a parameter, then invoked by that other function
 
+//higher order function is a function that accepts a callback function as a parameter
+
 function callback() {
   console.log("Coming from callback");
 }
@@ -17,3 +19,48 @@ higherOrder(callback);
 //About to call callback
 //Coming from callback
 //Callback has been invoked
+
+
+//What are callbacks used for?
+//Advanced Array Methods
+//Browser events
+//AJAX requests
+//React development
+
+
+//Code reuse with callbacks
+function sendMessage(message, callback) {
+  return callback(message);
+}
+
+sendMessage("message for console", console.log);
+sendMessage("message for alert", alert);
+let answer = sendMessage("Are you sure?", confirm);
+
+
+//Callbacks with function declarations
+function greet(name, formatter) {
+  return `Hello, ${formatter(name)}`;
+}
+
+function upperCaseName(name) {
+  return name.toUpperCase();
+}
+
+//returns "Hello, DANNY"
+greet("Danny", upperCaseName);
+
+//Callbacks with anonymous function - common pattern in JavaScript code bases
+function hello(name, formatter) {
+  return `Hello, ${formatter(name)}`;
+}
+
+//returns "Hello, DANNY"
+hello("Danny", function(name) {
+  return name.toUpperCase();
+});
+
+//returns "Hello, Danny!!!"
+hello("Danny",function(name) {
+  return name + "!!!";
+})
